ea6c0f872fe64314581de0f680d85d2ef1a9d4d3
David Ikharo <daveikharo@yahoo.com>
2023-03-28 04:51:28 -0700

    update

--- get_precision.c
+++ get_precision.c
@@ -10,40 +10,53 @@
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+    int curr_i = *i + 1;
+    int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+    if (format[curr_i] != '.')
+        return (precision);
 
-	precision = 0;
+    precision = 0;
 
-	// Handle the case when there is no precision specifier after the period
-	if (!is_digit(format[curr_i + 1]))
-		return (precision);
+    curr_i++;
+    if (format[curr_i] == '-')
+    {
+        precision = -1;
+        curr_i++;
+    }
 
-	// Handle the case when the precision specifier is a negative number
-	if (format[curr_i + 1] == '-')
-		return (precision);
+    for (; format[curr_i] != '\0'; curr_i++)
+    {
+        if (is_digit(format[curr_i]))
+        {
+            precision *= 10;
+            precision += format[curr_i] - '0';
+        }
+        else if (format[curr_i] == '*')
+        {
+            curr_i++;
+            precision = va_arg(list, int);
+            break;
+        }
+        else
+            break;
+    }
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+    if (precision < -1)
+        precision = -1;
 
-	*i = curr_i - 1;
+    if (precision == -1 && (format[curr_i] == 'f' || format[curr_i] == 'e' ||
+                            format[curr_i] == 'E' || format[curr_i] == 'g' ||
+                            format[curr_i] == 'G'))
+        precision = 6;
 
-	return (precision);
+    if (format[curr_i] != 'd' && format[curr_i] != 'i' && format[curr_i] != 'o' &&
+        format[curr_i] != 'u' && format[curr_i] != 'x' && format[curr_i] != 'X' &&
+        format[curr_i] != 'f' && format[curr_i] != 'e' && format[curr_i] != 'E' &&
+        format[curr_i] != 'g' && format[curr_i] != 'G' && format[curr_i] != 's')
+        precision = -1;
+
+    *i = curr_i - 1;
+
+    return (precision);
 }
